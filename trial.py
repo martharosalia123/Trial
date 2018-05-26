d45b23e3b3551baab5e0ae69b059b6d19a4ba3f4
martharosalia123 <marthanyambura029@gmail.com>
2018-05-26 04:39:12 -0700

    create new file

---
+++ trial.py
@@ -0,0 +1,18 @@
+list1 = [1,2,3,4,5]
+print(list1)
+
+list2 = ['Martha', 'Loryne', 'Faith']
+print(list2)
+
+list3 = [1,2,3, 'Martha', 'Loryne', 'Faith']
+print(list3)
+
+tuple1= ('banana', 'mango', 'apple')
+print(tuple1)
+
+list_4 = [2,5,6, 'Martha', 'Faith', 'Loryne', 'Martha',5,6,2, 'Faith']
+list_set = set(list_4)
+print(list_set)
+
+dict = {'Name':'Martha','Age':20,'Nationality':'Kenyan','Address':'Nairobi'}
+print(dict)
